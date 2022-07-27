08ea7387d01e165da2bf9e661d6f87be8f19f361
jam4u0 <jam4u0@gmail.com>
2022-07-27 19:35:01 +0900

    first build

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,21 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
+  outputDir: 'docs',
+  pages: {
+    index: {
+      entry: 'src/main.ts',
+      title: 'Fakegirl Clock',
+    }
+  },
+  publicPath: process.env.NODE_ENV === 'production' ? '/fakegirl-clock/' : '/',
+  pwa: {
+    name: 'Fakegirl Clock',
+    manifestOptions: {
+      short_name: 'Fakegirl Clock',
+      orientation: "portrait"
+    }
+  },
   transpileDependencies: [
     'vuetify'
-  ]
+  ],
 })
